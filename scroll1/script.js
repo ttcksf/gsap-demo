41d66cbb39000573daeb32a143cbac072760af05
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-10-01 14:43:48 +0900

    scroll1

--- scroll1/script.js
+++ scroll1/script.js
@@ -9,12 +9,5 @@ gsap.from('.page2 .box', {
   duration: 2,
   delay: 1,
   rotate: 360,
-  // scrollTrigger: '.page2 .box',
-  scrollTrigger: {
-    trigger: '.page2 .box',
-    scroller: 'body',
-    markers: true,
-    // スタートラインを画面の上からの座標で変更できる
-    start: 'top 60%',
-  },
+  scrollTrigger: '.page2 .box',
 });
